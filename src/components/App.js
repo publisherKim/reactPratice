4732f31b6ce14b1596c412aa1c0ed1126b2da3cc
JOEN <JOEN>
2017-01-06 14:50:53 +0900

    module

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
+import Header from './Header';
+import Content from './Content';
 
 class App extends React.Component {
     sayHey(){
         alert('hey');
     }
     render(){
-        let text = "Dev-Server";
-        let pStyle = {
-            color: 'aqua',
-            backgroundColor: 'black'
-        };
         return (
             <div>
-                <h1>Hello React</h1>
-                <h2>Welcome to {text}</h2>
-                {/* method를 생성하고 사용하는 방법 */} 
-                <button onClick={this.sayHey}>Click Me</button>
-                {/*if else 대신 삼항식*/}
-                <p style={pStyle}>{1 == 2 ? 'True' : 'False'}</p>
+                <Header/>
+                <Content/>
             </div>
         );
     }
